87bc0e2eea481644765b32f6c5819c3976b084d5
vinayakdp <143714589+vinayakdp@users.noreply.github.com>
2023-08-31 17:32:36 +0530

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+    float d= 34.4F;
+    long double e = 34.4L;
+    cout<<"34.4  "<<sizeof(34.4)<<endl;
+     cout<<"34.4f  "<<sizeof(34.4f)<<endl;
+      cout<<"34.4F  "<<sizeof(34.4F)<<endl;
+       cout<<"34.4l  "<<sizeof(34.4l)<<endl;
+        cout<<"34.4L  "<<sizeof(34.4L)<<endl;
+        return 0;
+}
